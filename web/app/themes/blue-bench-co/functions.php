a343a978fd2320f36b6dfec0a90255a6f8cc4023
Vincent Maglione <vincent@bluebench.co>
2024-05-28 20:37:09 -0400

    Enqueue theme style

--- web/app/themes/blue-bench-co/functions.php
+++ web/app/themes/blue-bench-co/functions.php
@@ -2,3 +2,15 @@
 
 namespace BlueBench;
 
+function enqueue_theme_assets() {
+	\wp_enqueue_style(
+		'blue-bench-co-style',
+		\get_stylesheet_uri()
+	);
+}
+\add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_theme_assets' );
+
+function enqueue_editor_assets() {
+	\add_editor_style(\get_parent_theme_file_uri('style.css'));
+}
+\add_action( 'after_setup_theme', __NAMESPACE__ . '\enqueue_editor_assets' );
